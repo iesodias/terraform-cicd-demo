38b76d6dd0fcfc8d50a4794e34bcaf857355873f
Ieso Dias <iesodias@gmail.com>
2023-01-22 21:32:02 -0300

    automated commit

--- manifest/main.tf
+++ manifest/main.tf
@@ -1,10 +1,18 @@
 terraform {
-  cloud {
-    organization = "devops-actions"
-
-    workspaces {
-      name = "terraform-cicd-demo"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
     }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformgithubactionsid"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
